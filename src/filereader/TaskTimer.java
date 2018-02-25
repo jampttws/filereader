4c7c02c108f96cc330648594e2e9cd0bcc63837a
jampttws <tanasorn42@hotmail.com>
2018-02-25 13:20:01 +0700

    update javadoc

--- src/filereader/TaskTimer.java
+++ src/filereader/TaskTimer.java
@@ -1,10 +1,15 @@
 package filereader;
 
-
+/**
+ * This class can compute and print the elapsed time for any task.
+ * @author Tanasorn Tritawisup
+ *
+ */
 public class TaskTimer{
 
 	private static Stopwatch watch = new Stopwatch();
 	
+	/**Measure the time that use to read a file*/
 	public static void measure(Runnable task){
 		watch.start();
 		task.run();
